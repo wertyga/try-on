304c1e25112c354684dd143406b6a83ff96a66ef
alexejbronshtein <wertyga@github.com>
2026-02-28 20:32:47 +0100

    iap and apple oauth

--- api/base/base-query.utils.ts
+++ api/base/base-query.utils.ts
@@ -1,6 +1,9 @@
 export const buildAPIError = (e: any) => {
   const message =
-    e.response?.data?.error?.message || e.response?.data?.message || e.message;
+    e.response?.data?.error?.message ||
+    e.response?.data?.message ||
+    e.response?.data?.error ||
+    e.message;
 
   const status =
     e.response?.data?.error?.status || e.response?.status || e.status || 500;
